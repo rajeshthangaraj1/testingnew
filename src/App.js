380dbd12d59455e87d1fc4d76049a4bc17e1de35
rajeshthangaraj1 <rajeshthangaraj1@gmail.com>
2018-10-07 12:25:41 +0400

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            rajesh
           </p>
           <a
             className="App-link"
